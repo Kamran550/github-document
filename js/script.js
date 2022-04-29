0c166e9b3a8cb2f0aa3526456a569eaad990b037
Your Name <you@example.com>
2022-04-30 00:46:24 +0400

    salam

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
-  function clicked(e)
-  {
-    const elementLi = document.querySelectorAll('ul li')
-    foreach (elem in elementLi)
-    {
-      elem.classList.remove('active')
-      elem[0].classList.remove('activeTag')
-      elem.classList.remove('activeLeft')
-      elem.classList.remove('activeRight')
-    }
-    e.classList.add('active')
-    e[0].classList.add('activeTag')
-    e.previousSibling.classList.add('activeLeft')
-    e.nextSibling.classList.add('activeRight')
-    alert(e)
-  console.log(e)
-  console.log(e[0])
-  console.log(e.previousSibling)
-  console.log(e.nextSibling)
-  }
+  // function clicked(e)
+  // {
+  //   const elementLi = document.querySelectorAll('ul li')
+  //   foreach (elem in elementLi)
+  //   {
+  //     elem.classList.remove('active')
+  //     elem[0].classList.remove('activeTag')
+  //     elem.classList.remove('activeLeft')
+  //     elem.classList.remove('activeRight')
+  //   }
+  //   e.classList.add('active')
+  //   e[0].classList.add('activeTag')
+  //   e.previousSibling.classList.add('activeLeft')
+  //   e.nextSibling.classList.add('activeRight')
+  //   alert(e)
+  // console.log(e)
+  // console.log(e[0])
+  // console.log(e.previousSibling)
+  // console.log(e.nextSibling)
+  // }
+
+
